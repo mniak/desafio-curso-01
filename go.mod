4d3b57b8d512467b09219a9f0894c8dc13f4abd5
Andre Soares <andregsilv@gmail.com>
2020-04-13 10:55:15 -0300

    Funcionando

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mniak/desafio-curso-01
 
 go 1.14
 
-require github.com/go-resty/resty/v2 v2.2.0
+require (
+	github.com/go-resty/resty/v2 v2.2.0
+	github.com/manifoldco/promptui v0.7.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	golang.org/x/sys v0.0.0-20190530182044-ad28b68e88f1 // indirect
+)
--- main.go
+++ main.go
@@ -1,53 +1,105 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"sync"
 
 	"github.com/go-resty/resty/v2"
+	"github.com/manifoldco/promptui"
 )
 
 type hcResult struct {
 	IsHealthy bool
 }
 
-func check(wg *sync.WaitGroup, url string) {
+type site struct {
+	Name      string
+	Domain    string
+	SubDomain string
+}
+
+func (s *site) url(sandbox bool) string {
+	if sandbox {
+		return fmt.Sprintf("https://%ssandbox.%s/healthcheck", s.SubDomain, s.Domain)
+	}
+	return fmt.Sprintf("https://%s.%s/healthcheck", s.SubDomain, s.Domain)
+}
+
+func check(wg *sync.WaitGroup, s site, sandbox bool) {
 	defer wg.Done()
 
 	client := resty.New()
 	resp, err := client.R().
 		SetResult(&hcResult{}).
-		Get(url)
+		Get(s.url(sandbox))
 
-	// resp, err := http.Get(url)
 	if err != nil {
-		log.Printf("Error while fetching hc %v: %v\n", url, err)
+		fmt.Printf("Erro ao testar site  %v: %v\n", s.Name, err)
 		return
 	}
 
 	result := resp.Result().(*hcResult)
 	if result.IsHealthy {
-		log.Printf("Site %v is healthy\n", url)
+		fmt.Printf("%v está saudável\n", s.Name)
 	} else {
-		log.Printf("Site %v is not healthy\n", url)
+		fmt.Printf("%v está com erro\n", s.Name)
 	}
+}
+
+func askSandbox() bool {
+
+	prompt := promptui.Select{
+		Label: "Ambiente",
+		Items: []string{"Sandbox", "Produção"},
+	}
+	n, _, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Erro na leitura %v. Usando sandbox\n", err)
+		n = 1
+	}
+	fmt.Printf("N selecionado: %d\n", n)
+	sandbox := n == 0
+	return sandbox
 }
 
 func main() {
 
-	urls := []string{
-		"https://parametersdownloadsandbox.cieloecommerce.cielo.com.br/healthcheck",
-		"https://merchantapisandbox.cieloecommerce.cielo.com.br/healthcheck",
-		"https://omnisandbox.cieloecommerce.cielo.com.br/healthcheck",
-		"https://omniquerysandbox.cieloecommerce.cielo.com.br/healthcheck",
-		"https://onboardingsandbox.cieloecommerce.cielo.com.br/healthcheck",
+	sites := []site{
+		{
+			Name:      "Omni API",
+			Domain:    "cieloecommerce.cielo.com.br",
+			SubDomain: "omni",
+		},
+		{
+			Name:      "Omni Query API",
+			Domain:    "cieloecommerce.cielo.com.br",
+			SubDomain: "omniquery",
+		},
+		{
+			Name:      "Parameters Download API",
+			Domain:    "cieloecommerce.cielo.com.br",
+			SubDomain: "parametersdownload",
+		},
+		{
+			Name:      "Merchants API",
+			Domain:    "cieloecommerce.cielo.com.br",
+			SubDomain: "merchantapi",
+		},
+		{
+			Name:      "Onboarding API",
+			Domain:    "cieloecommerce.cielo.com.br",
+			SubDomain: "onboarding",
+		},
 	}
-	var wg sync.WaitGroup
 
-	for _, d := range urls {
+	sandbox := askSandbox()
+
+	var wg sync.WaitGroup
+	for _, s := range sites {
 		wg.Add(1)
-		go check(&wg, d)
+		go check(&wg, s, sandbox)
 	}
 
 	wg.Wait()
+	fmt.Println("-- That's all folks! --")
 }
