4b72c0de4774eb5689109998b46e9369311ab3f7
Andre Soares <andregsilv@gmail.com>
2020-04-16 15:48:01 -0300

    Use config.go

---
+++ config.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/viper"
+)
+
+//Site represents one site for which run the health check
+type Site struct {
+	Name      string
+	Domain    string
+	SubDomain string
+}
+
+func (s *Site) url(isprod bool) string {
+	if isprod {
+		return fmt.Sprintf("https://%s.%s/healthcheck", s.SubDomain, s.Domain)
+	}
+	return fmt.Sprintf("https://%ssandbox.%s/healthcheck", s.SubDomain, s.Domain)
+}
+
+//Config represents the configuration file
+type Config struct {
+	Sites []Site
+}
+
+func readConfig() (config Config, err error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	if viper.Unmarshal(&config); err != nil {
+		return
+	}
+
+	return
+}
+
+func useProd() bool {
+
+	prodPtr := flag.Bool("prod", false, "Use production environment")
+	sboxPtr := flag.Bool("sbox", false, "Use sandbox environment")
+	flag.Parse()
+
+	flagProd, flagSbox := *prodPtr, *sboxPtr
+
+	if flagSbox || flagProd {
+		return flagProd && !flagSbox
+	}
+	prompt := promptui.Select{
+		Label:     "Ambiente",
+		Items:     []string{"Sandbox", "Produção"},
+		IsVimMode: false,
+	}
+	n, _, err := prompt.Run()
+	if err != nil {
+		log.Printf("Erro na leitura %v. Usando sandbox\n", err)
+		n = 0
+	}
+	return n == 1
+}
--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"sync"
 
 	"github.com/go-resty/resty/v2"
-	"github.com/manifoldco/promptui"
-	"github.com/spf13/viper"
 )
 
 type hcResult struct {
 	IsHealthy bool
 }
 
-func (s *site) url(prod bool) string {
-	if prod {
-		return fmt.Sprintf("https://%s.%s/healthcheck", s.SubDomain, s.Domain)
-	}
-	return fmt.Sprintf("https://%ssandbox.%s/healthcheck", s.SubDomain, s.Domain)
-}
-
-func check(wg *sync.WaitGroup, s site, prod bool) {
+func check(wg *sync.WaitGroup, s Site, prod bool) {
 	defer wg.Done()
 
 	client := resty.New()
@@ -43,62 +32,15 @@ func check(wg *sync.WaitGroup, s site, prod bool) {
 	}
 }
 
-func determineIfProd(flagSbox, flagProd bool) bool {
-
-	if flagSbox || flagProd {
-		return flagProd && !flagSbox
-	}
-	prompt := promptui.Select{
-		Label:     "Ambiente",
-		Items:     []string{"Sandbox", "Produção"},
-		IsVimMode: false,
-	}
-	n, _, err := prompt.Run()
-	if err != nil {
-		log.Printf("Erro na leitura %v. Usando sandbox\n", err)
-		n = 0
-	}
-	return n == 1
-}
-
-type site struct {
-	Name      string
-	Domain    string
-	SubDomain string
-}
-
-//Config represents the configuration file
-type Config struct {
-	Sites []struct {
-		Name      string
-		Domain    string
-		SubDomain string
-	}
-}
-
 func main() {
 
-	var err error
-
-	prodPtr := flag.Bool("prod", false, "Use production environment")
-	sboxPtr := flag.Bool("sbox", false, "Use sandbox environment")
-	flag.Parse()
-
-	viper.SetConfigName("config")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath(".")
-
-	if err = viper.ReadInConfig(); err != nil {
-		log.Fatalln(err)
-	}
-
-	var config Config
-	if viper.Unmarshal(&config); err != nil {
+	config, err := readConfig()
+	if err != nil {
 		log.Fatalln(err)
 	}
 
 	sites := config.Sites
-	prod := determineIfProd(*sboxPtr, *prodPtr)
+	prod := useProd()
 
 	var wg sync.WaitGroup
 	for _, s := range sites {
